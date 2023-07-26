8ce7d905d33a229eddb8c9e113137e03ddbb90ef
Kourtney Monroe <mrs.monroe@Kourtneys-MacBook-Air.local>
2023-07-26 18:16:45 -0500

    App section complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import Banner from "./components/Banner";
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
+import { useState } from "react";
+import satData from "./components/satData";
+
 
 function App() {
+    const [sat, setSat] = useState(satData);
+    const displaySats = [...new Set(satData.map((data) => data.orbitType))];
+    filterByType = (currentType) => {
+      const displaySats = satData.filter((newSatDisplay) => {
+        return newSatDisplay.orbitType === currentType;
+      });
+      setSat(displaySats);
+    };// code continues ...
   return (
     <div>
       <Banner />
-      <Buttons />
-      <Table />
+      <Buttons 
+      filterByType={filterByType}
+      setSat={setSat}
+      displaySats={displaySats}
+      />
+      <Table sat={sat} />
     </div>
   );
 }
